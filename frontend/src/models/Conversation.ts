9387b1be904fe3aa9d949c4bfea963a4e6a0349b
Subham Kumar Paul <subham.ku.paul@gmail.com>
2022-12-02 23:56:21 +0530

    fourth

--- frontend/src/models/Conversation.ts
+++
@@ -1,9 +0,0 @@
-import { MessageModel } from './Message';
-import  UserModel  from './User';
-
-export interface ConversationModel {
-  id: string;
-  name: string;
-  last_message: MessageModel | null;
-  other_user: UserModel;
-}
